41c9991ee5b923ddef018476292b77bb35f1a42f
Leandro Toledo <leandrotoledodesouza@gmail.com>
2016-04-23 10:35:48 -0300

    Update example.py
    changes name to first_name

--- website/static/website/example.py
+++ website/static/website/example.py
@@ -5,7 +5,7 @@ def start(bot, update):
 
 def hello(bot, update):
     bot.sendMessage(update.message.chat_id,
-                    text='Hello {0}'.format(update.message.from_user.name))
+                    text='Hello {0}'.format(update.message.from_user.first_name))
 
 updater = Updater('YOUR TOKEN HERE')
 
