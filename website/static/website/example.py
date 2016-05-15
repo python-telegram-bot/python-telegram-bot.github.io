e7b9877bf0ab18a844a8390de0903357fddbab64
Leandro Toledo <leandrotoledodesouza@gmail.com>
2016-05-15 20:32:25 -0300

    Update example.py
    s/sendMessage/send_message/g
    s/addHandler/add_handler/g

--- website/static/website/example.py
+++ website/static/website/example.py
@@ -1,16 +1,16 @@
 from telegram.ext import Updater, CommandHandler
 
 def start(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Hello World!')
+    bot.send_message(update.message.chat_id, text='Hello World!')
 
 def hello(bot, update):
-    bot.sendMessage(update.message.chat_id,
+    bot.send_message(update.message.chat_id,
                     text='Hello {0}'.format(update.message.from_user.first_name))
 
 updater = Updater('YOUR TOKEN HERE')
 
-updater.dispatcher.addHandler(CommandHandler('start', start))
-updater.dispatcher.addHandler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
 updater.start_polling()
 updater.idle()
