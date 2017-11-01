76ecb1a0bb4096390269b3a0ce9491d927b6611e
Jannes Höke <jhoeke@uni-bremen.de>
2017-11-01 16:12:30 +0100

    create real template, add sponsors page

--- ptb_website/urls.py
+++ ptb_website/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('website.urls'))
+    url(r'^', include('website.urls'))
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'website/index.html', {})
+
+def page(request, page):
+    return render(request, 'website/%s.html' % page, {})
