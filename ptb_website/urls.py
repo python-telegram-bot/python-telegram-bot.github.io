e1c7428855ef098a1c86d8a5cec62c3a470a8e07
Jannes Höke <mail@jhoeke.de>
2019-08-23 23:47:50 +0200

    :lock: Upgrade Django to newest version

--- ptb_website/urls.py
+++ ptb_website/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^', include('website.urls'))
 ]
