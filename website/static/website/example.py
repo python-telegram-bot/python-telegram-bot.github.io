f7dab809fad2dce3364aa2093eb13a81a8e51c57
poolitzer <deadpool10@web.de>
2019-09-14 21:53:20 -0700

    adding Pygments to requirements, update example to context

--- website/static/website/example.py
+++ website/static/website/example.py
@@ -1,15 +1,13 @@
 from telegram.ext import Updater, CommandHandler
 
-def start(bot, update):
-    update.message.reply_text('Hello World!')
 
-def hello(bot, update):
+def hello(update, context):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
 
+
 updater = Updater('YOUR TOKEN HERE')
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
 updater.start_polling()
