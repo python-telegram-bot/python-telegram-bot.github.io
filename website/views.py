ef283400847f3d21398b614922ab0258c486ef1b
poolitzer <github@poolitzer.eu>
2022-05-12 16:37:27 +0200

    rewrite to static html files

--- website/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request):
-    return render(request, 'website/index.html', {})
-
-def page(request, page):
-    return render(request, 'website/%s.html' % page, {})
